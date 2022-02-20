154c095cbd2131f6150818454364e8dd3a45cd3e
inzomnia <inzomnia7@gmail.com>
2022-02-20 14:22:37 +0100

    save highScore even after game quits

--- scoreboard.py
+++ scoreboard.py
@@ -5,8 +5,12 @@ FONT = ('Courier', 23, 'normal')
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        # setup playerscore and get highScore from saveFile'highscore.txt'
         self.player_score = 0
-        self.high_score = 0
+        with open("highscore.txt", mode="r") as saveFile:
+            self.saved_highscore = saveFile.read()
+
+        # Setup scoreBoard
         self.penup()
         self.pencolor("white")
         self.setposition(0, 270)
@@ -17,11 +21,16 @@ class Scoreboard(Turtle):
         self.player_score += 1
 
     def print_scoreboard(self):
-        self.clear()  # removes the previous scoreboard from screen to prevent overlays
-        self.write(f"Score:{self.player_score}   HighScore:{self.high_score}", False, align="center", font=FONT)
+        """Updates the scoreBoard"""
+        self.clear()
+        self.write(f"Score:{self.player_score}   HighScore:{self.saved_highscore}", False, align="center", font=FONT)
 
     def reset(self):
-        if self.player_score > self.high_score:
-            self.high_score = self.player_score
+        # if playerScore is bigger than highScore: new highScore
+        if self.player_score > int(self.saved_highscore):
+            self.saved_highscore = self.player_score
+            # save new highscore even after gameExit, in highscore.txt
+            with open("highscore.txt", mode="w") as saveFile:
+                saveFile.write(str(self.saved_highscore))
         self.player_score = 0
         self.print_scoreboard()
